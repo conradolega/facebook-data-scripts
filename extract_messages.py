a4fcef3095473ac12f99497870356bd8919306ed
Conrad Olega <conrad@conradolega.xyz>
2018-03-30 17:17:43 +0800

    Extract message dates

--- extract_messages.py
+++ extract_messages.py
@@ -2,13 +2,19 @@ import os
 import pandas
 from bs4 import BeautifulSoup
 
-filename = ''
+filename = '/home/conradolega/Downloads/facebook-data/messages/296.html'
 
 with open(filename) as f:
     soup = BeautifulSoup(f.read(), 'html.parser')
     # Messages are archived in descending chronological order
     message_headers = soup.find_all('div', class_='message')
     message_headers.reverse()
-    messages = [{'user': message_header.find('span').text, 'message': message_header.next_sibling.text} for message_header in message_headers]
+    messages = [
+        {
+            'user': message_header.find('span', class_='user').text,
+            'date': message_header.find('span', class_='meta').text,
+            'message': message_header.next_sibling.text,
+        }
+        for message_header in message_headers]
     df = pandas.DataFrame.from_dict(messages)
     df.to_csv('messages.csv', encoding='utf-8')
